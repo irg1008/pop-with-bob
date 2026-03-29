72b769041da45ea81bf8cc13aee25f89ce05b8a4
irg1008 <ivansudevlop@gmail.com>
2026-03-30 01:03:58 +0200

    config(project): move player to separate collision layer

--- assets/scripts/weapons/projectile.gd
+++ assets/scripts/weapons/projectile.gd
@@ -11,7 +11,6 @@ func _physics_process(delta: float) -> void:
 	var end_pos: Vector3 = global_position + velocity * delta
 
 	var query: PhysicsRayQueryParameters3D = PhysicsRayQueryParameters3D.create(start_pos, end_pos)
-	query.collision_mask = 1
 	var result: Dictionary = space_state.intersect_ray(query)
 
 	if not result:
