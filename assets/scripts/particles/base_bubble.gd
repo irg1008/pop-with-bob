38eabdffea414632e1a75f557cfb2bd1bc3513bb
irg1008 <ivansudevlop@gmail.com>
2026-04-01 14:51:23 +0200

    feat(bubble): move logic to parent

--- assets/scripts/particles/base_bubble.gd
+++ assets/scripts/particles/base_bubble.gd
@@ -1,10 +1,12 @@
-class_name Bubble extends RigidBody3D
+class_name BaseBubble extends Node3D
 
 
 @export_category("References")
 @export var animation_player: AnimationPlayer
+@export var rigid_body: RigidBody3D
 @export var pop_effect: GPUParticles3D
 
+@export_category("Bubble Properties")
 @export var wobble_strength: float = 0.5
 @export var max_lifetime: float = 10.0
 
@@ -13,21 +15,19 @@ var time_alive: float = 0.0
 
 
 func _ready() -> void:
-	if not animation_player:
-		return
-
-	freeze = true
-	sleeping = true
-	contact_monitor = false
+	rigid_body.freeze = true
+	rigid_body.sleeping = true
+	rigid_body.contact_monitor = false
 
 	animation_player.play("inflate")
 	animation_player.animation_finished.connect(_on_inflate_animation_finished)
 
-	body_entered.connect(_on_body_entered)
+	rigid_body.body_entered.connect(_on_body_entered)
+	pop_effect.finished.connect(_on_pop_effect_finished)
 
 
 func _physics_process(delta: float) -> void:
-	if freeze:
+	if not rigid_body or rigid_body.freeze:
 		return
 
 	time_alive += delta
@@ -37,16 +37,17 @@ func _physics_process(delta: float) -> void:
 		return
 
 	var random_direction: Vector3 = Vector3(randf_range(-1, 1), 0, randf_range(-1, 1)).normalized()
-	apply_central_force(random_direction * wobble_strength)
+	rigid_body.apply_central_force(random_direction * wobble_strength)
 
 
 func _on_inflate_animation_finished(_animation_name: String) -> void:
-	freeze = false
-	sleeping = false
-	contact_monitor = true
-	max_contacts_reported = 1
+	rigid_body.freeze = false
+	rigid_body.sleeping = false
+	rigid_body.contact_monitor = true
+	rigid_body.max_contacts_reported = 1
 
-	apply_central_impulse(Vector3(0, 0, 0.01))
+	rigid_body.apply_central_impulse(Vector3.FORWARD * 0.01)
+
 
 func _on_body_entered(_body: Node) -> void:
 	pop()
@@ -57,6 +58,10 @@ func _on_health_component_died() -> void:
 
 
 func pop() -> void:
-	pop_effect.global_transform = global_transform
+	pop_effect.global_position = rigid_body.global_position
 	pop_effect.restart()
+	rigid_body.queue_free()
+
+
+func _on_pop_effect_finished() -> void:
 	queue_free()
