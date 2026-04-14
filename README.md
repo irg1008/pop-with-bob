2cf2261499a934cbf89d7837f3aabe2ff707ba10
irg1008 <ivansudevlop@gmail.com>
2026-04-14 18:41:04 +0200

    feat(hose): update hose tool with new water drop shader and enhanced spray parameters

--- README.md
+++ README.md
@@ -5,3 +5,5 @@
 
 Characters: CC0 1.0 - https://quaternius.com/packs/cutemonsters.html
 Audio effects: CC0 1.0 - https://pixabay.com/
+
+Water sound: Watering.wav + Unity C# Code by wobesound -- https://freesound.org/s/488401/ -- License: Creative Commons 0
--- assets/scripts/tools/hose.gd
+++ assets/scripts/tools/hose.gd
@@ -1,10 +1,14 @@
-extends Node
+extends Node3D
+
+
+@export var max_flow_amount: float = 1.0
 
 
 @onready var watering_area: Area3D = $WateringArea
 @onready var water_spray: GPUParticles3D = $WaterSpray
 
 
+var current_flow: float = 0.0
 var pouring: bool = false
 
 
@@ -15,10 +19,15 @@ func _ready() -> void:
 func _physics_process(delta: float) -> void:
 	pouring = Input.is_action_pressed("pour_water")
 
-	water_spray.emitting = pouring
-
 	if pouring:
 		_pour_water(delta)
+		current_flow = lerp(current_flow, max_flow_amount, delta * 3.0)
+	else:
+		current_flow = lerp(current_flow, 0.0, delta * 10.0)
+
+	water_spray.amount_ratio = current_flow
+	water_spray.process_material.scale_min = 0.05 + (current_flow * 0.03)
+	water_spray.emitting = current_flow > 0.01
 
 
 func _pour_water(delta: float) -> void:
