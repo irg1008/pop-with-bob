1618534bc47ddd7e521421fbb19ef473fc16927a
irg1008 <ivansudevlop@gmail.com>
2026-04-16 11:29:03 +0200

    feat(hose): refactor HoseAnchor and create PlayerHose scene

--- assets/scripts/tools/hose_anchor.gd
+++ assets/scripts/tools/hose_anchor.gd
@@ -1,10 +1,17 @@
 class_name HoseAnchor extends Node
 
 
-@export var hose: RigidHose3D
+@export var attachment_start: Node3D
+
+
+@onready var hose: RigidHose3D = $RigidHose3D
 
 
 func _ready() -> void:
+	print("HoseAnchor ready with attachment_start: ", attachment_start)
+	if attachment_start:
+		hose.attachment_start = attachment_start.get_path()
+
 	set_player_as_anchor_end.call_deferred()
 
 
